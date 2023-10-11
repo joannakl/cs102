8d7667564ebbcf39602e2d536818708aca10bcb2
Joanna Kl <joannakl@cs.nyu.edu>
2023-10-11 16:57:53 -0400

    fix formatting

--- projects/project3_logs/project3/SortedLinkedList.java
+++ projects/project3_logs/project3/SortedLinkedList.java
@@ -4,18 +4,18 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 /**
- * This is an implementation of a sorted doubly-linked list. 
+ * This is an implementation of a sorted doubly-linked list.
  * All elements in the list are maintained in ascending/increasing order
  * based on the natural order of the elements.
- * This list does not allow <code>null</code> elements. 
- * 
+ * This list does not allow <code>null</code> elements.
+ *
  * @author Joanna Klukowska
- * @author 
- * 
+ * @author
+ *
  * @param <E> the type of elements held in this list
  */
 public class SortedLinkedList<E extends Comparable<E>>
-        implements Iterable<E> {
+    implements Iterable<E> {
 
     private Node head;
     private Node tail;
@@ -25,16 +25,16 @@ public class SortedLinkedList<E extends Comparable<E>>
      * Constructs a new empty sorted linked list.
      */
     public SortedLinkedList() {
-    	head = null;
-    	tail = null;
-    	size = 0;
+        head = null;
+        tail = null;
+        size = 0;
     }
 
     /**
      * Adds the specified element to the list in ascending order.
      *
      * @param element the element to add
-     * @return <code>true</code> if the element was added successfully, 
+     * @return <code>true</code> if the element was added successfully,
      * <code>false</code> otherwise (if <code>element==null</code>)
      */
     public boolean add(E element) {
@@ -50,11 +50,11 @@ public class SortedLinkedList<E extends Comparable<E>>
     }
 
     /**
-     * Returns <code>true</code> if the list contains the specified element, 
+     * Returns <code>true</code> if the list contains the specified element,
      * <code>false</code> otherwise.
      *
      * @param o the element to search for
-     * @return <code>true</code> if the element is in the list, 
+     * @return <code>true</code> if the element is in the list,
      * <code>false</code> otherwise
      */
     public boolean contains(Object o) {
@@ -67,8 +67,8 @@ public class SortedLinkedList<E extends Comparable<E>>
      *
      * @param index the index of the element to return
      * @return the element at the specified index
-	 * @throw IndexOutOfBoundsException  if the index is out of 
-	 * range <code>(index < 0 || index >= size())</code>
+     * @throw IndexOutOfBoundsException  if the index is out of
+     * range <code>(index < 0 || index >= size())</code>
      */
     public E get(int index) throws IndexOutOfBoundsException {
         // TODO: Implement this method
@@ -80,7 +80,7 @@ public class SortedLinkedList<E extends Comparable<E>>
      * or -1 if the element is not in the list.
      *
      * @param o the element to search for
-     * @return the index of the first occurrence of the element, 
+     * @return the index of the first occurrence of the element,
      * or -1 if the element is not in the list
      */
     public int indexOf(Object o) {
@@ -90,8 +90,8 @@ public class SortedLinkedList<E extends Comparable<E>>
 
     /**
      * Returns the index of the first occurrence of the specified element in the list,
-     * starting at the specified <code>index</code>, i.e., in the range of indexes 
-     * <code>index <= i < size()</code>, or -1 if the element is not in the list 
+     * starting at the specified <code>index</code>, i.e., in the range of indexes
+     * <code>index <= i < size()</code>, or -1 if the element is not in the list
      * in the range of indexes <code>index <= i < size()</code>.
      *
      * @param o the element to search for
@@ -138,7 +138,7 @@ public class SortedLinkedList<E extends Comparable<E>>
      * Compares the specified object with this list for equality.
      *
      * @param o the object to compare with
-     * @return <code>true</code> if the specified object is equal to this list, 
+     * @return <code>true</code> if the specified object is equal to this list,
      * <code>false</code> otherwise
      */
     public boolean equals(Object o) {
@@ -147,10 +147,10 @@ public class SortedLinkedList<E extends Comparable<E>>
     }
 
     /**
-     * Returns a string representation of the list. 
-     *  The string representation consists of a list of the lists's elements in 
-     *  ascending order, enclosed in square brackets ("[]"). 
-     *  Adjacent elements are separated by the characters ", " (comma and space). 
+     * Returns a string representation of the list.
+     *  The string representation consists of a list of the lists's elements in
+     *  ascending order, enclosed in square brackets ("[]").
+     *  Adjacent elements are separated by the characters ", " (comma and space).
      *
      * @return a string representation of the list
      */
@@ -158,45 +158,45 @@ public class SortedLinkedList<E extends Comparable<E>>
         // TODO: Implement this method
         return "[]";
     }
-	
-    /* Inner class to represent nodes of this list.*/
-	private class Node implements Comparable<Node>{
-		E data; 
-		Node next;
-		Node prev;
-		Node(E data) {
-			if (data == null ) throw new NullPointerException ("does not allow null");
-			this.data = data; 
-		}
-		Node (E data, Node next, Node prev) {
-			this(data);
-			this.next = next;
-			this.prev = prev;
-		}
-		public int compareTo( Node n ) {
-			return this.data.compareTo(n.data); 
-		}
-	}
-	
-	/* A basic forward iterator for this list. */
-	private class ListIterator implements Iterator<E> {
-		
-		Node nextToReturn = head; 
-		@Override
-		public boolean hasNext() {
-			return nextToReturn != null;
-		}
 
-		@Override
-		public E next() throws NoSuchElementException{
-            if (nextToReturn == null ) 
+    /* Inner class to represent nodes of this list.*/
+    private class Node implements Comparable<Node> {
+        E data;
+        Node next;
+        Node prev;
+        Node(E data) {
+            if (data == null ) throw new NullPointerException ("does not allow null");
+            this.data = data;
+        }
+        Node (E data, Node next, Node prev) {
+            this(data);
+            this.next = next;
+            this.prev = prev;
+        }
+        public int compareTo( Node n ) {
+            return this.data.compareTo(n.data);
+        }
+    }
+
+    /* A basic forward iterator for this list. */
+    private class ListIterator implements Iterator<E> {
+
+        Node nextToReturn = head;
+        @Override
+        public boolean hasNext() {
+            return nextToReturn != null;
+        }
+
+        @Override
+        public E next() throws NoSuchElementException {
+            if (nextToReturn == null )
                 throw new NoSuchElementException("the end of the list reached");
             E tmp = nextToReturn.data;
             nextToReturn = nextToReturn.next;
-			return tmp;
-		}
-		
-	}
+            return tmp;
+        }
+
+    }
 }
 
 
